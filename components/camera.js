f4ea46a825fb11ea5f8f98b1b2322e7b5aae4b08
Yasuhiro <yasuhiro1026a@hotmail.com>
2018-05-29 00:26:28 +0900

    first commit to ccamera

---
+++ components/camera.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera, Permissions } from 'expo';
+import isIphoneX from './isIphoneX';
+
+export default class CCamera extends React.Component {
+    state = {
+        hasCameraPermission: null,
+        type: Camera.Constants.Type.back,
+    };
+
+    async componentWillMount() {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({ hasCameraPermission: status === 'granted' });
+    }
+
+    render() {
+        const { hasCameraPermission } = this.state;
+        if (hasCameraPermission === null) {
+            return (
+                <View>
+                    <Text style={styles.CameraText}>
+                        no camera parmission
+                    </Text>
+                </View>
+            );
+        } else if (hasCameraPermission === false) {
+            return (
+                <View>
+                    <Text style={styles.CameraText}>No access to camera</Text>
+                </View>
+            );
+        } else {
+            return (
+                <View style={styles.cameraContainer}>
+                    <Camera style={{ flex: 1 }} type={this.state.type}>
+                        <View style={styles.cameraStyle}>
+                            <TouchableOpacity
+                                style={styles.TouchableOpacityStyle}
+                                onPress={() => {
+                                    this.setState({
+                                        type: this.state.type === Camera.Constants.Type.back
+                                            ? Camera.Constants.Type.front
+                                            : Camera.Constants.Type.back,
+                                    });
+                                }}>
+                                <Text
+                                    style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                                    {' '}Flip{' '}
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </Camera>
+                </View>
+            );
+        }
+
+    }
+}
+
+const styles = StyleSheet.create({
+    cameraContainer: {
+        flex: 1,
+    },
+    cameraStyle: {
+        flex: 1,
+        backgroundColor: 'transparent',
+        flexDirection: 'row',
+    },
+    TouchableOpacityStyle: {
+        flex: 0.1,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+    },
+    CameraText: {
+        backgroundColor: "blue",
+        flex: 1,
+        color: "yellow",
+        fontSize: 25,
+    },
+});
