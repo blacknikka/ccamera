d7fd3e11eb47f1fce0cbab08b54c556b685aded0
Yasuhiro Nishihara <yasuhiro@YasuMac.local>
2018-05-31 21:12:38 +0900

    add react-navigator

--- App.js
+++ App.js
@@ -1,35 +1,35 @@
+import { StackNavigator } from 'react-navigation';
 import React from 'react';
 import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 import CCamera from './components/camera'
 
 
 export default class App extends React.Component {
-  state = {
-    counter: 0
-  }
+    state = {
+        counter: 0
+    }
 
-  ChangeText = () => {
-    this.setState(x => {
-      return { counter: x.counter + 1 }
-    });
-  }
+    ChangeText = () => {
+        this.setState(x => {
+            return { counter: x.counter + 1 }
+        });
+    }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <CCamera />
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <CCamera />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 35,
-    paddingBottom: 18,
-    backgroundColor: '#ddd',
-    alignItems: 'stretch',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        paddingTop: 35,
+        backgroundColor: '#ddd',
+        alignItems: 'stretch',
+        justifyContent: 'center',
+    },
 });
